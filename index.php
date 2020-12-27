b5015dde757665b1463db218bad04de7c5409986
MaureenJoy <73148238+MaureenJoy@users.noreply.github.com>
2020-12-27 11:21:39 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
-<?php include_once("index.html"); ?>
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Log in form</title>
+	<meta name="viewport" content="width=device-width,initial-scale=1.0"/>
+	<meta http-equiv="X-UA-Compatible" content="ied=edge"/>
+	<link rel="stylesheet" type="text/css" href="try.css">
+</head>
+<body method="post" action="try.php">
+	<form>
+		<input type="text" placeholder="enter username" name="username"><br>
+		<input type="text" placeholder="enter password" name="pwd"><br>
+		<button type="submit" name="submit">submit</button>
+	</form>
+</body>
+</html>
